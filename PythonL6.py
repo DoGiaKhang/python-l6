27d517a8650b9c458c9c858e4b4129c9e6c7e3d6
DoGiaKhang <164725044+DoGiaKhang@users.noreply.github.com>
2024-03-31 14:39:29 +0700

    Add files via upload

---
+++ PythonL6.py
@@ -0,0 +1,13 @@
+#Bai1
+i = int(input("Nhap so nguyen: "))
+if i%2!=0 and i%3!=0 and i%5!=0 and i%7 and i>1:
+    print(i,"la so nguyen to")
+elif i == 1:
+    print("i la 1")
+elif i == 0:
+    print("i la 0")
+else:
+    print(i,"khong phai la so nguyen to")
+
+#Bai2
+    
